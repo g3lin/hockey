88e1fd926fe121ed3e8f34251140e23bccced192
Antoine Gélin <gelinantoine@gmail.com>
2019-10-04 15:14:23 -0400

    Added Ktor, Gradle and general Kotlin boilerplate for the server part

---
+++ server/src/Application.kt
@@ -0,0 +1,21 @@
+package ca.udes
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import java.io.*
+import java.util.*
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.network.util.*
+import kotlin.coroutines.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.io.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+}
+

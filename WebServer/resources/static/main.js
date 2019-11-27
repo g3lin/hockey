8671c6625631709e8c0685148e27fd5db067a766
Antoine GÉLIN <gelinantoine@gmail.com>
2019-11-26 20:20:28 -0500

    Suivi des matchs et interface fonctionels et finis

--- WebServer/resources/static/main.js
+++ WebServer/resources/static/main.js
@@ -1,36 +1,63 @@
 function updateMatch(matchID, data){
-    document.getElementById(matchID+"-E1").innerHTML = "Sherbrooke"
-    document.getElementById(matchID+"-E2").innerHTML = "Sherbrooke"
-    document.getElementById(matchID+"-E1b").innerHTML = "Sherbrooke"
-    document.getElementById("1-E2b").innerHTML = "Sherbrooke"
+    console.log(matchID+"-E1")
+    document.getElementById(matchID+"-E1").innerHTML = data["nomEquipe1"]
+    document.getElementById(matchID+"-E2").innerHTML = data["nomEquipe2"]
+    document.getElementById(matchID+"-E1b").innerHTML = data["nomEquipe1"]
+    document.getElementById(matchID+"-E2b").innerHTML = data["nomEquipe2"]
 
-    document.getElementById(matchID+"-chrono").innerHTML = "00:01"
+    document.getElementById(matchID+"-chrono").innerHTML = Math.floor(parseInt(data["chronometreSec"])/60)+":"+parseInt(data["chronometreSec"])%60
+    document.getElementById(matchID+"-periode").innerHTML = data["PeriodeEnCours"]
 
-    document.getElementById(matchID+"-ScoreP1E1").innerHTML = "1"
-    document.getElementById(matchID+"-ScoreP2E1").innerHTML = "1"
-    document.getElementById(matchID+"-ScoreP3E1").innerHTML = "1"
-    document.getElementById(matchID+"-ScoreTotE1").innerHTML = "3"
+    document.getElementById(matchID+"-ScoreP1E1").innerHTML = data["scoreP1"].split(",")[0]
+    document.getElementById(matchID+"-ScoreP2E1").innerHTML = data["scoreP2"].split(",")[0]
+    document.getElementById(matchID+"-ScoreP3E1").innerHTML = data["scoreP3"].split(",")[0]
+    document.getElementById(matchID+"-ScoreTotE1").innerHTML = parseInt(data["scoreP1"].split(",")[0])+parseInt(data["scoreP2"].split(",")[0])+parseInt(data["scoreP3"].split(",")[0])
 
-    document.getElementById(matchID+"-ScoreP1E2").innerHTML = "1"
-    document.getElementById(matchID+"-ScoreP2E2").innerHTML = "1"
-    document.getElementById(matchID+"-ScoreP3E2").innerHTML = "1"
-    document.getElementById(matchID+"-ScoreTotE2").innerHTML = "3"
+    document.getElementById(matchID+"-ScoreP1E2").innerHTML = data["scoreP1"].split(",")[1].trim()
+    document.getElementById(matchID+"-ScoreP2E2").innerHTML = data["scoreP2"].split(",")[1].trim()
+    document.getElementById(matchID+"-ScoreP3E2").innerHTML = data["scoreP3"].split(",")[1].trim()
+    document.getElementById(matchID+"-ScoreTotE2").innerHTML = parseInt(data["scoreP1"].split(",")[1].trim()) + parseInt(data["scoreP2"].split(",")[1].trim()) + parseInt(data["scoreP3"].split(",")[1].trim())
 
-    document.getElementById("1-pena").innerHTML = "Carton jaune"
+    document.getElementById(matchID+"-pena").innerHTML = data["penalites"]
 }
 
-function updateAll(){
 
+
+function UpdateAll(){
     var xmlhttp = new XMLHttpRequest();
-    var Url = "/api/ListeDesMatchs";
+    var Url = "/api/match?query={\"objectType\":\"request\",\"objectRequested\":\"ListeDesMatchs\", \"idObjectRequested\":\"\"}";
     xmlhttp.open("GET", Url);
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    // xmlhttp.send(JSON.stringify({"objectType":"request","objectRequested":"ListeDesMatchs", "idObjectRequested":""}));
-    xmlhttp.send()
-
-
-    updateMatch(1)    
+    xmlhttp.send();
+    xmlhttp.onload=function(){
+        json=JSON.parse(xmlhttp.responseText);
+        console.log(JSON.stringify(json));
+        json.matchIDs.forEach(mID => queryMatch(mID))
+        
+        if(json["matchIDs"].length<5){document.getElementById("match5").hidden = true}
+        if(json["matchIDs"].length<4){document.getElementById("match4").hidden = true}
+        if(json["matchIDs"].length<3){document.getElementById("match3").hidden = true}
+        if(json["matchIDs"].length<2){document.getElementById("match2").hidden = true}
+      };
 }
+
+
+function queryMatch(matchID){
+    var xmlhttp = new XMLHttpRequest();
+    var Url = "/api/match?query={\"objectType\":\"request\",\"objectRequested\":\"Match\", \"idObjectRequested\":\""+matchID+"\"}";
+    xmlhttp.open("GET", Url);
+    xmlhttp.send();
+    xmlhttp.onload=function(){
+        json=JSON.parse(xmlhttp.responseText);
+        console.log(JSON.stringify(json));
+        updateMatch(matchID, json["match"])
+        
+      };
+
+}
+
+
+
+
 
 function processBet(){
 
@@ -39,4 +66,4 @@ function processBet(){
 
 
 
-updateAll()
+UpdateAll()
